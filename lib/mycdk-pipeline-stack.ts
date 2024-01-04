bf8c1be6f488a5e0f49ba507c9964b946938b89e
Parag <paragrajput52@gmail.com>
2024-01-04 16:47:12 +0530

    First Commit

--- lib/mycdk-pipeline-stack.ts
+++ lib/mycdk-pipeline-stack.ts
@@ -9,7 +9,7 @@ export class MycdkPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('paragrajput/mycdk-pipeline', 'master'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
