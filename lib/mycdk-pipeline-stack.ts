3edd6a81f0f663c9bce991da464dbce49e671704
Parag <paragrajput52@gmail.com>
2024-02-23 18:59:46 +0530

    add source

--- lib/mycdk-pipeline-stack.ts
+++ lib/mycdk-pipeline-stack.ts
@@ -6,17 +6,17 @@ import { MycdkPipelineStage } from './mycdk-pipeline-stage';
 export class MycdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    const source = CodePipelineSource.gitHub('paragrajput/mycdk-pipeline','master');
     const pipeline = new CodePipeline(this, 'MyPipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('paragrajput/mycdk-pipeline', 'master'),
+        input: source,
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
-      pipeline.addStage(new MycdkPipelineStage (this,"DockerBuild",{
-        env:{ account:"634842168668",region:"ap-south-1"}
-      }));
+      // pipeline.addStage(new MycdkPipelineStage (this,"DockerBuild",{
+      //   env:{ account:"634842168668",region:"ap-south-1"}
+      // }));
   }
 }
 
